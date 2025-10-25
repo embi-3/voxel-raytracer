79d2854fca15460e51fb74715f18e9d3d1b10398
Leo Mai <z5593164@ad.unsw.edu.au>
2025-10-25 11:41:45 +1100

    clang-format and style changes

---
+++ src/classes/colour.hpp
@@ -0,0 +1,69 @@
+#ifndef COLOUR_H
+#define COLOUR_H
+
+#include "common.hpp"
+#include "vec3.hpp"
+
+#include <cstdint>
+
+namespace texture {
+    class Colour {
+    public:
+        uint8_t r = 0;
+        uint8_t g = 0;
+        uint8_t b = 0;
+        num a = 0; // Alpha channel, range: [0, 1]
+
+        explicit constexpr Colour() noexcept = default;
+
+        explicit constexpr Colour(const num r, const num g, const num b, const num a = 1) noexcept
+        : r(r)
+        , g(g)
+        , b(b)
+        , a(a) {}
+
+        Colour& operator+=(const Colour& rhs) {
+            // TODO: Implement RGBA addition logic.
+            return *this;
+        }
+
+        // ! Remove this function if unneeded.
+        // Vec3 rgb() {
+        // 	return Vec3(r, g, b);
+        // }
+
+        static constexpr Colour black() noexcept {
+            return Colour(0, 0, 0);
+        }
+
+        static constexpr Colour white() noexcept {
+            return Colour(255, 255, 255);
+        }
+
+        static constexpr Colour red() noexcept {
+            return Colour(255, 0, 0);
+        }
+
+        static constexpr Colour green() noexcept {
+            return Colour(0, 255, 0);
+        }
+
+        static constexpr Colour blue() noexcept {
+            return Colour(0, 0, 255);
+        }
+
+        static constexpr Colour yellow() noexcept {
+            return Colour(255, 255, 0);
+        }
+
+        static constexpr Colour cyan() noexcept {
+            return Colour(0, 255, 255);
+        }
+
+        static constexpr Colour magenta() noexcept {
+            return Colour(255, 0, 255);
+        }
+    };
+} // namespace texture
+
+#endif // COLOUR_H
---
+++ src/classes/voxel.hpp
@@ -0,0 +1,24 @@
+#ifndef VOXEL_H
+#define VOXEL_H
+
+#include "colour.hpp"
+#include "common.hpp"
+#include "material.hpp"
+#include "vec3.hpp"
+
+#include <array>
+
+constexpr int NUM_FACES = 6;
+
+namespace geometry {
+    class Voxel {
+    public:
+        geometry::Vec3 coords = geometry::Vec3();
+        std::array<texture::Colour, NUM_FACES> colours =
+            {Colour(), Colour(), Colour(), Colour(), Colour(), Colour()}; // ! This is really bad code.
+        texture::Material material = Material();
+
+        explicit constexpr Voxel() = default;
+    };
+} // namespace geometry
+#endif // VOXEL_H
