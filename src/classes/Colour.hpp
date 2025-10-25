608ba8950b6e08493dd56e612e29c5d11bb150c8
Leo Mai <z5593164@ad.unsw.edu.au>
2025-10-25 11:28:22 +1100

    Added namespaces and minor edits

--- src/classes/Colour.hpp
+++ src/classes/Colour.hpp
@@ -4,59 +4,61 @@
 #include "Common.hpp"
 #include "Vec3.hpp"
 
-class Colour {
-	public:
-		// ? Should we make these int8_t for compactness?
-		int r = 0;
-		int g = 0;
-		int b = 0;
-		num a = 0; // Alpha channel, range: [0, 1]
+#include <cstdint>
 
-		explicit constexpr Colour() = default;
-        explicit constexpr Colour(const num r, const num g, const num b) : r(r), g(g), b(b), a(1) {}
-        explicit constexpr Colour(const num r, const num g, const num b, const num a) : r(r), g(g), b(b), a(a) {}
-		
-		Colour& operator+=(const Colour& rhs) {
-			// TODO: Implement RGBA addition logic.
-			return *this;
-		}
-		
-		// ! Remove this function if unneeded.
-		// Vec3 rgb() {
-		// 	return Vec3(r, g, b);
-		// }
+namespace texture {
+	class Colour {
+		public:
+			uint8_t r = 0;
+			uint8_t g = 0;
+			uint8_t b = 0;
+			num a = 0; // Alpha channel, range: [0, 1]
 
-		static Colour black() {
-			return Colour(0, 0, 0);
-		}
+			explicit constexpr Colour() noexcept = default;
+			explicit constexpr Colour(const num r, const num g, const num b, const num a = 1) noexcept : r(r), g(g), b(b), a(a) {}
 
-		static Colour white() {
-			return Colour(255, 255, 255);
-		}
+			Colour& operator+=(const Colour& rhs) {
+				// TODO: Implement RGBA addition logic.
+				return *this;
+			}
+			
+			// ! Remove this function if unneeded.
+			// Vec3 rgb() {
+			// 	return Vec3(r, g, b);
+			// }
 
-		static Colour red() {
-			return Colour(255, 0, 0);
-		}
+			static constexpr Colour black() noexcept {
+				return Colour(0, 0, 0);
+			}
 
-		static Colour green() {
-			return Colour(0, 255, 0);
-		}
+			static constexpr Colour white() noexcept {
+				return Colour(255, 255, 255);
+			}
 
-		static Colour blue() {
-			return Colour(0, 0, 255);
-		}
+			static constexpr Colour red() noexcept {
+				return Colour(255, 0, 0);
+			}
 
-		static Colour yellow() {
-			return Colour(255, 255, 0);
-		}
+			static constexpr Colour green() noexcept {
+				return Colour(0, 255, 0);
+			}
 
-		static Colour cyan() {
-			return Colour(0, 255, 255);
-		}
+			static constexpr Colour blue() noexcept {
+				return Colour(0, 0, 255);
+			}
 
-		static Colour magenta() {
-			return Colour(255, 0, 255);
-		}
-};
+			static constexpr Colour yellow() noexcept {
+				return Colour(255, 255, 0);
+			}
+
+			static constexpr Colour cyan() noexcept {
+				return Colour(0, 255, 255);
+			}
+
+			static constexpr Colour magenta() noexcept {
+				return Colour(255, 0, 255);
+			}
+	};
+}
 
 #endif // COLOUR_H
