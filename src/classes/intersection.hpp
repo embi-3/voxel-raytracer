188bfba4b54798e4d51021fe652004abe53f0094
embi-3 <139082906+embi-3@users.noreply.github.com>
2025-10-27 17:13:55 +1100

    Simple ray caster (#2)
    * Add initial implementation of Vec3 class
    
    * Minor comments and adjustments
    
    * Added skeleton classes and converted spaces to tabs
    
    * Added namespaces and minor edits
    
    * clang-format and style changes
    
    * Fixed error where constructor was accepting wrong types for rgb values.
    
    * Void casted variable to avoid unused variable warnings
    
    * Minor fixes in vec3 and moved some logic to a cpp file
    
    * Fixed header statements
    
    * Implemented basic PPM image generator
    
    * Fixed header file for static_renderer
    
    * simple ray caster with camera and viewport
    
    ---------
    
    Co-authored-by: Leo Mai <z5593164@ad.unsw.edu.au>
    Co-authored-by: Charlie <z5689107@ad.unsw.edu.au>

---
+++ src/classes/intersection.hpp
@@ -0,0 +1,21 @@
+#ifndef INTERSECTION_H
+#define INTERSECTION_H
+
+#include "../common.hpp"
+#include "vec3.hpp"
+#include "colour.hpp"
+#include "material.hpp"
+#include "voxel.hpp"
+
+namespace geometry {
+    class Intersection {
+        public:
+            texture::Colour colour;
+            // texture::Material material;
+            // num distance;
+    };
+
+
+}
+
+#endif // INTERSECTION_H
---
+++ src/classes/voxel.hpp
@@ -0,0 +1,25 @@
+#ifndef VOXEL_H
+#define VOXEL_H
+
+#include "colour.hpp"
+#include "../common.hpp"
+#include "material.hpp"
+#include "vec3.hpp"
+
+#include <array>
+
+constexpr int NUM_FACES = 6;
+
+namespace geometry {
+    class Voxel {
+        using Faces = std::array<texture::Colour, NUM_FACES>;
+
+    public:
+        geometry::Vec3 coords = geometry::Vec3();
+        Faces colours = {Colour(), Colour(), Colour(), Colour(), Colour(), Colour()}; // ! This is really bad code.
+        texture::Material material = Material();
+
+        explicit constexpr Voxel() = default;
+    };
+} // namespace geometry
+#endif // VOXEL_H
