082a0458b0fd0abaa7e0b5cdcb2cf4ca3ed1f432
embi-3 <139082906+embi-3@users.noreply.github.com>
2025-10-27 12:01:32 +1100

    Added skeleton classes and converted spaces to tabs

---
+++ src/classes/VoxelGrid.hpp
@@ -0,0 +1,29 @@
+#ifndef VOXELGRID_H
+#define VOXELGRID_H
+
+#include "Voxel.hpp"
+#include <cmath>
+#include <iostream>
+
+// #include <vector>
+// using std::vector;
+
+#define MAX_WORLD_SIZE 100
+
+class VoxelGrid {
+    public:
+        // Create a sparse 3D array for storing voxels.
+        Voxel ***world;
+        int world_size = 32;
+
+        explicit VoxelGrid() {
+            world = new Voxel**[world_size];
+        }
+
+        explicit VoxelGrid(int world_size_) {
+            world = new Voxel**[world_size_];
+            world_size = world_size_;
+        }
+};
+
+#endif // VOXELGRID_H
