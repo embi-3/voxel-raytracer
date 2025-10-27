864ecfbe1cd68ed6394d74ae699c7bdc19828389
embi-3 <139082906+embi-3@users.noreply.github.com>
2025-10-27 11:06:11 +1100

    Added skeleton classes and converted spaces to tabs

---
+++ src/classes/Material.hpp
@@ -0,0 +1,14 @@
+#ifndef MATERIAL_H
+#define MATERIAL_H
+
+#include "Common.hpp"
+#include "Vec3.hpp"
+
+struct Material {
+    public:
+        // Add more fields here.
+
+        explicit constexpr Material() = default;
+};
+
+#endif // MATERIAL_H
