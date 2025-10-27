3f3c7afb2e64035ad2a70f9788f90c8abf4e748a
embi-3 <139082906+embi-3@users.noreply.github.com>
2025-10-27 17:13:55 +1100

    Simple ray caster (#2)
    * Add initial implementation of Vec3 class
    
    * Minor comments and adjustments
    
    * Added skeleton classes and converted spaces to tabs
    
    * Added namespaces and minor edits
    
    * clang-format and style changes
    
    * Fixed error where constructor was accepting wrong types for rgb values.
    
    * Void casted variable to avoid unused variable warnings
    
    * Minor fixes in vec3 and moved some logic to a cpp file
    
    * Fixed header statements
    
    * Implemented basic PPM image generator
    
    * Fixed header file for static_renderer
    
    * simple ray caster with camera and viewport
    
    ---------
    
    Co-authored-by: Leo Mai <z5593164@ad.unsw.edu.au>
    Co-authored-by: Charlie <z5689107@ad.unsw.edu.au>

---
+++ src/classes/vec3.hpp
@@ -0,0 +1,117 @@
+// Based on code from https://raytracing.github.io/books/RayTracingInOneWeekend.html
+
+#ifndef VEC3_H
+#define VEC3_H
+
+#include "../common.hpp"
+#include <cmath>
+#include <iostream>
+
+namespace geometry {
+    class Vec3 {
+    public:
+        // Zero constructor
+        explicit constexpr Vec3() noexcept = default;
+
+        explicit constexpr Vec3(const num val) noexcept
+        : x(val)
+        , y(val)
+        , z(val) {}
+
+        explicit constexpr Vec3(const num x, const num y, const num z) noexcept
+        : x(x)
+        , y(y)
+        , z(z) {}
+
+        Vec3 operator-() const noexcept {
+            return Vec3(-x, -y, -z);
+        }
+
+        Vec3& operator+=(const Vec3& rhs) noexcept {
+            x += rhs.x;
+            y += rhs.y;
+            z += rhs.z;
+            return *this;
+        }
+
+        Vec3& operator-=(const Vec3& rhs) noexcept {
+            return *this += -rhs;
+        }
+
+        Vec3& operator*=(num t) noexcept {
+            x *= t;
+            y *= t;
+            z *= t;
+            return *this;
+        }
+
+        Vec3& operator/=(num t) {
+            return (*this *= (static_cast<num>(1) / t));
+        }
+
+        num length() const noexcept {
+            return std::sqrt(length_squared());
+        }
+
+        num length_squared() const noexcept {
+            return (x * x) + (y * y) + (z * z);
+        }
+
+        Vec3 into_unit() const noexcept;    // defined outside class (at the bottom)
+
+        num dot(const Vec3& rhs) const noexcept {
+            return (x * rhs.x) + (y * rhs.y) + (z * rhs.z);
+        }
+
+        Vec3 cross(const Vec3& rhs) const noexcept {
+            return Vec3((y * rhs.z) - (z * rhs.y), (z * rhs.x) - (x * rhs.z), (x * rhs.y) - (y * rhs.x));
+        }
+
+        // ! Double check this does what it should.
+        Vec3 reflect(const Vec3& n) const;
+
+        // ? Should this function name be shortened?
+        Vec3 into_reciprocal() const {
+            return Vec3(1 / x, 1 / y, 1 / z);
+        }
+
+        // Components
+        num x = 0;
+        num y = 0;
+        num z = 0;
+    };
+
+    // point3 is just an alias for vec3, but useful for geometric clarity in the code.
+    using Point3 = Vec3;
+
+    // Vector Non-Member Utility Functions
+    inline std::ostream& operator<<(std::ostream& out, const Vec3& v) {
+        return out << "Vec3(" << v.x << ", " << v.y << ", " << v.z << ")";
+    }
+
+    inline Vec3 operator+(const Vec3& u, const Vec3& v) noexcept {
+        return Vec3(u.x + v.x, u.y + v.y, u.z + v.z);
+    }
+
+    inline Vec3 operator-(const Vec3& u, const Vec3& v) noexcept {
+        return Vec3(u.x - v.x, u.y - v.y, u.z - v.z);
+    }
+
+    inline Vec3 operator*(num t, const Vec3& v) noexcept {
+        return Vec3(t * v.x, t * v.y, t * v.z);
+    }
+
+    inline Vec3 operator*(const Vec3& v, num t) noexcept {
+        return t * v;
+    }
+
+    inline Vec3 operator/(const Vec3& v, num t) {
+        return (static_cast<num>(1) / t) * v;
+    }
+
+    inline Vec3 Vec3::into_unit() const noexcept {
+        return *this / length();
+    }
+} // namespace geometry
+
+#endif
