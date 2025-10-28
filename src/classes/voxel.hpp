db690b9ff1bceb89a0335bbf493f20f44a03ba06
embi-3 <139082906+embi-3@users.noreply.github.com>
2025-10-28 13:55:44 +1100

    Add unsaved changes

--- src/classes/voxel.hpp
+++ src/classes/voxel.hpp
@@ -19,7 +19,6 @@ constexpr int NUM_FACES = 6;
 
 namespace geometry {
     class Voxel {
-<<<<<<< HEAD
         public:
             bool opaque = true;
             // geometry::Vec3 coords = geometry::Vec3(); // ! We shouldn't store the coordinates for the voxel in itself.
@@ -29,16 +28,4 @@ namespace geometry {
             explicit constexpr Voxel() = default;
     };
 }
-=======
-        using Faces = std::array<texture::Colour, NUM_FACES>;
-
-    public:
-        geometry::Vec3 coords = geometry::Vec3();
-        // Faces colours = {}; // ! This is really bad code.
-        texture::Material material = texture::Material();
-
-        explicit constexpr Voxel() = default;
-    };
-} // namespace geometry
->>>>>>> main
 #endif // VOXEL_H
