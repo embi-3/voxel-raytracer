4c7755239e9bfd9c7583c3aef1287c1a347d2b6e
embi-3 <139082906+embi-3@users.noreply.github.com>
2025-10-30 20:49:34 +1100

    Implement binary operations for Colour

--- src/classes/colour.hpp
+++ src/classes/colour.hpp
@@ -9,18 +9,25 @@
 namespace texture {
 	class Colour {
 		public:
-			uint8_t r = 0;
-			uint8_t g = 0;
-			uint8_t b = 0;
+			num r = 0;
+			num g = 0;
+			num b = 0;
 			num a = 0; // Alpha channel, range: [0, 1]
 
 			explicit constexpr Colour() noexcept = default;
-			explicit constexpr Colour(const uint8_t r, const uint8_t g, const uint8_t b, const num a = 1) noexcept : r(r), g(g), b(b), a(a) {}
+			explicit constexpr Colour(const num r, const num g, const num b, const num a = 1) noexcept : r(r), g(g), b(b), a(a) {}
 
 			Colour& operator+=([[maybe_unused]] const Colour& rhs) {
 				// TODO: Implement RGBA addition logic.
 				return *this;
 			}
+
+            Colour& operator*=(num s) noexcept {
+                r *= s;
+                g *= s;
+                b *= s;
+                return *this;
+            }
 			
 			// ! Remove this function if unneeded.
 			// Vec3 rgb() {
@@ -60,7 +67,15 @@ namespace texture {
 			}
 	};
 
-	// TODO: Implement binary operands for scalar multiplication of Colours
+    // ! This currently takes the average of the alpha channel for both colours.
+    // ! How this implementation should handle addition should be discussed later.
+    inline Colour operator+(const Colour& u, const Colour& v) noexcept {
+        return Colour(u.r + v.r, u.g + v.g, u.b + v.b, (u.a + v.a) / 2);
+    }
+
+    inline Colour operator*(num s, const Colour& c) noexcept {
+        return Colour(s*c.r, s*c.g, s*c.b, c.a);
+    }
 }
 
 #endif // COLOUR_H
