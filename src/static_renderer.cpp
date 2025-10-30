3eda773d4b5f0cea7bac609144ab54da6f67c7cc
embi-3 <139082906+embi-3@users.noreply.github.com>
2025-10-30 14:50:36 +1100

    Fix make errors

--- src/static_renderer.cpp
+++ src/static_renderer.cpp
@@ -30,7 +30,7 @@ void createPPM(std::size_t width, std::size_t height, const std::vector<Pixel>& 
 
 // simple gradient from 4.2 from "Ray Tracing In One Weekend" website
 texture::Colour ray_colour(const geometry::Ray& r) {
-    geometry::Vec3 unit_direction = r.direction().into_unit();
+    geometry::Vec3 unit_direction = r.dir.normalise();
     auto a = 0.5 * (unit_direction.y + 1.0);
     return (1.0 - a) * texture::Colour(1.0, 1.0, 1.0) + a * texture::Colour(0.5, 0.7, 1.0);
 }
