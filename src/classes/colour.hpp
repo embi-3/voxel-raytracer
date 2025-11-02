a866ec46d0e6c8bf34a146343574cf6c77356362
embi-3 <139082906+embi-3@users.noreply.github.com>
2025-11-02 12:13:56 +1100

    Restore unbounded operator overloads

--- src/classes/colour.hpp
+++ src/classes/colour.hpp
@@ -94,6 +94,13 @@ namespace texture {
 				return std::to_string(r_int()) + " " + std::to_string(g_int()) + " " + std::to_string(b_int()) + " " + std::to_string(a);
 			}
 
+			// inline bool is_valid() const {
+			// 	return (r >= 0 && r < 256)
+			// 		&& (g >= 0 && g < 256)
+			// 		&& (b >= 0 && b < 256)
+			// 		&& (a >= 0.0 && a <= 1.0);
+			// }
+
 			// static_cast always truncates the floating part of the number, rounding down.
 
 			inline uint8_t r_int() const {
@@ -113,6 +120,9 @@ namespace texture {
 	// ! The following operations always return a valid RGBA value. If you are getting unexpected
 	// ! results, check for possible overflows in your arithmetic. You should avoid calling these
 	// ! operators directly and use blend modes instead.
+
+	// ? Does clamping on every operation have a significant performance penalty? 
+	// ? This could be something to test.
 
     inline Colour operator+(const Colour& u, const Colour& v) noexcept {
 		return Colour(
@@ -140,6 +150,42 @@ namespace texture {
 			c.a
 		);
     }
+
+	// Unbounded Operator Overloads
+	// ! These have been left here for future testing purposes.
+
+	// // ! These operations may produce invalid RGBA values! Be careful of overflows!
+    // inline Colour operator+(const Colour& u, const Colour& v) noexcept {
+    //     Colour res = Colour(u.r + v.r, u.g + v.g, u.b + v.b, max((u.a) + (u.a * v.a));
+	// 	if (!res.is_valid()) {
+	// 		std::cerr << "Invalid RGBA value! Received " << res.to_rgb_string();
+	// 		return Colour::black();
+	// 	}
+
+	// 	return res;
+    // }
+
+	// // ! These operations may produce invalid RGBA values! Be careful of overflows!
+    // inline Colour operator*(const num s, const Colour& c) noexcept {
+    //     Colour res = Colour(s*c.r, s*c.g, s*c.b, c.a);
+	// 	if (!res.is_valid()) {
+	// 		std::cerr << "Invalid RGBA value! Received " << res.to_rgba_string();
+	// 		return Colour::black();
+	// 	}
+
+	// 	return res;
+    // }
+
+	// // ! These operations may produce invalid RGBA values! Be careful of overflows!
+	// inline Colour operator*(const Colour& c, const num s) noexcept {
+    //     Colour res = Colour(s*c.r, s*c.g, s*c.b, c.a);
+	// 	if (!res.is_valid()) {
+	// 		std::cerr << "Invalid RGBA value! Received " << res.to_rgba_string();
+	// 		return Colour::black();
+	// 	}
+
+	// 	return res;
+    // }
 	
 	num bounded_rgb(num value) {
 		return std::clamp(value, RGB_MIN, RGB_MAX);
