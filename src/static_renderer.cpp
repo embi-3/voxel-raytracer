13be213f3664213515dedb6f72d968181613003f
embi-3 <139082906+embi-3@users.noreply.github.com>
2025-11-16 17:14:04 +1100

    Add test scene

--- src/static_renderer.cpp
+++ src/static_renderer.cpp
@@ -1,6 +1,9 @@
 #include "static_renderer.hpp"
+#include "classes/camera.hpp"
 #include "classes/colour.hpp"
 #include "classes/ray.hpp"
+#include "classes/scene.hpp"
+#include "classes/shaders/shader.hpp"
 #include "classes/vec3.hpp"
 
 #include "stb_image_write.h"
@@ -12,10 +15,13 @@
 #include <vector>
 
 using Pixel = texture::Colour;
+using namespace texture;
+using namespace geometry;
+using namespace renderer;
 using namespace std::chrono;
 
 // Outputs PPM ASCII and PNG
-void create_image(std::size_t width, std::size_t height, const std::vector<Pixel>& pixels) {
+void create_png(std::size_t width, std::size_t height, const std::vector<Pixel>& pixels) {
     if (pixels.size() != width * height)
         throw std::invalid_argument("Error: Number of pixel entries does not match the expected size.");
 
@@ -42,58 +48,47 @@ void create_image(std::size_t width, std::size_t height, const std::vector<Pixel
                    static_cast<int>(channels * width));
 }
 
-// simple gradient from 4.2 from "Ray Tracing In One Weekend" website
-Pixel gradient(const geometry::Ray& r) {
-    geometry::Vec3 unit_direction = r.dir.normalise();
-    auto a = 0.5 * (unit_direction.y + 1.0);
-    return texture::interpolate(Pixel(124, 179, 255), Pixel::white(), a);
-}
-
 int main() {
+    // ! DEBUG
+    std::cout << "> Starting...\n";
     const auto start = high_resolution_clock::now();
 
-    // 16:9 aspect ratio, width of 400
-    auto aspect_ratio = 16.0 / 9.0;
     int image_width = 400;
+    int image_height = 200;
 
-    // finds height from ratio and width (height at least 1)
-    int image_height = int(image_width / aspect_ratio);
-    image_height = (image_height < 1) ? 1 : image_height;
+    
+    // ! DEBUG
+    std::cout << "> Initialising camera...\n";
+    Camera camera = Camera(image_width, image_height);
 
-    // camera
-    auto focal_length = 1.0;
-    auto viewport_height = 2.0;
-    auto viewport_width = viewport_height * (double(image_width) / image_height);
-    auto camera_center = geometry::Vec3(0, 0, 0);
+    // Create the scene
+    // ! DEBUG
+    std::cout << "> Creating scene...\n";
+    Scene scene = Scene();
 
-    // calculate horizontal and vertical viewport vectors
-    auto viewport_u = geometry::Vec3(viewport_width, 0, 0);
-    auto viewport_v = geometry::Vec3(0, -viewport_height, 0);
+    // ! DEBUG
+    std::cout << "> Creating grid...\n";
+    VoxelGrid grid = VoxelGrid(10, Vec3(-5, -5, 5));
+    std::cerr << "min: " << grid.bounding_box.min << ", max: " << grid.bounding_box.max << "\n";
+    // grid.at(5, 5, 5) = Voxel();
+    
+    // TODO: Test shapes that go outside the boundary of the grid and see what happens.
+    std::cout << "> Creating shapes...\n";
+    grid.create_sphere(Coordinate(4, 6, 3), 5);
+    // grid.create_cube(Coordinate(5, 5, 5), 1);
 
-    // delta vectors from pixel to pixel
-    auto delta_u = viewport_u / image_width;
-    auto delta_v = viewport_v / image_height;
+    scene.push_back(grid);
 
-    // vector for upper left pixel
-    auto viewport_upper_left = camera_center - geometry::Vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
-    auto upper_left_pixel = viewport_upper_left + 0.5 * (delta_u + delta_v);
-
-    // render
-    auto pixels = std::vector<Pixel>{}; // array of pixels
-    for (int j = 0; j < image_height; j++) {
-        for (int i = 0; i < image_width; i++) {
-            auto pixel_center = upper_left_pixel + (i * delta_u) + (j * delta_v);
-            auto ray_direction = pixel_center - camera_center;
-            auto r = geometry::Ray{camera_center, ray_direction};
-
-            auto pixel_colour = gradient(r);
-            pixels.push_back(pixel_colour);
-        }
-    }
-    create_image(static_cast<std::size_t>(image_width), static_cast<std::size_t>(image_height), pixels);
+    // Render the scene
+    // ! DEBUG
+    std::cout << "> Rendering scene...\n";
+    GradientShader gradient_shader = GradientShader();
+    WhiteShader white_shader = WhiteShader();
+    auto pixels = camera.render(scene, white_shader);
+    create_png(static_cast<std::size_t>(image_width), static_cast<std::size_t>(image_height), pixels);
 
     const auto end = high_resolution_clock::now();
     const auto duration = duration_cast<milliseconds>(end - start).count();
 
-    std::cout << "Time taken: " << duration << " ms\n";
+    std::cout << "> Time taken: " << duration << " ms\n";
 }
